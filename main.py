331e2314d425f9c18c057858c4880644d7b12925
PiotrSzczepanski8 <114732037+PiotrSzczepanski8@users.noreply.github.com>
2025-04-15 19:25:23 +0200

    selecting pieces

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ field_size = 64
 background = pygame.image.load('images/n-background.png')
 background = pygame.transform.scale(background, (screen_width, screen_height))
 
+mouse_pos = [screen_width, screen_height]
 
 while running:
-    mouse_pos = [0, 0]
     
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
@@ -38,38 +38,86 @@ while running:
 
     screen.blit(background, (0, 0))
 
-    pygame.draw.rect(screen, red, pygame.Rect(board_left, board_top, field_size*8, field_size*8))
-    
-    '''alternative red field drawing for red fields selecting
-    for i in range(8):
-      for j in range(8):
-          field = pygame.Rect(board_left, board_top, field_size, field_size)
-    '''
-        
-
     # board
     j = 0
     while j<8:
-        board_left = 32+field_size
-        for i in range(4):
+        board_left = 32
+        for i in range(0, 8, 2):
           field = pygame.Rect(board_left, board_top, field_size, field_size)
           mouse_rect = pygame.Rect(mouse_pos[0], mouse_pos[1], 1, 1)
           if field.contains(mouse_rect):
-            mouse_pos = [i, j]
+            mouse_pos = [j, i]
             print(mouse_pos)
-          pygame.draw.rect(screen, navy_blue, field)
+          if board.board[j][i]:
+            if board.board[j][i].selected:
+              field_color = '#822C20'
+            else:
+              field_color = red
+          else:
+            field_color = red
+          pygame.draw.rect(screen, field_color, field)
+          board_left += field_size*2
+
+        j+=1
+        board_top += field_size
+        board_left = 32 + field_size
+        for i in range(1, 8, 2):
+          field = pygame.Rect(board_left, board_top, field_size, field_size)
+          mouse_rect = pygame.Rect(mouse_pos[0], mouse_pos[1], 1, 1)
+          if field.contains(mouse_rect):
+            mouse_pos = [j, i]
+            print(mouse_pos)
+          if board.board[j][i]:
+            if board.board[j][i].selected:
+              field_color = '#822C20'
+            else:
+              field_color = red
+          else:
+            field_color = red
+          pygame.draw.rect(screen, field_color, field)
+          board_left += field_size*2
+        j+=1
+        board_top += field_size
+    
+    board_left = 32
+    board_top = 80
+
+    j = 0
+    while j<8:
+        board_left = 32+field_size
+        for i in range(1, 8, 2):
+          field = pygame.Rect(board_left, board_top, field_size, field_size)
+          mouse_rect = pygame.Rect(mouse_pos[0], mouse_pos[1], 1, 1)
+          if field.contains(mouse_rect):
+            mouse_pos = [j, i]
+            print(mouse_pos)
+          if board.board[j][i]:
+            if board.board[j][i].selected:
+              field_color = '#887F9D'
+            else:
+              field_color = navy_blue
+          else:
+            field_color = navy_blue
+          pygame.draw.rect(screen, field_color, field)
           board_left += field_size*2
 
         j+=1
         board_top += field_size
         board_left = 32
-        for i in range(4):
+        for i in range(0, 8, 2):
           field = pygame.Rect(board_left, board_top, field_size, field_size)
           mouse_rect = pygame.Rect(mouse_pos[0], mouse_pos[1], 1, 1)
           if field.contains(mouse_rect):
-            mouse_pos = [i, j]
+            mouse_pos = [j, i]
             print(mouse_pos)
-          pygame.draw.rect(screen, navy_blue, field)
+          if board.board[j][i]:
+            if board.board[j][i].selected:
+              field_color = '#887F9D'
+            else:
+              field_color = navy_blue
+          else:
+            field_color = navy_blue
+          pygame.draw.rect(screen, field_color, field)
           board_left += field_size*2
         j+=1
         board_top += field_size
@@ -90,6 +138,20 @@ while running:
             image = pygame.transform.scale(image, (field_size, field_size))
 
             screen.blit(image, (board_left+(field_size*j), board_top+(field_size*i)))
+            
+            if [i, j] == mouse_pos:
+              board.board[i][j].selected = True
+            else:
+              board.board[i][j].selected = False
+              
+    '''
+    for i in range(8):
+      for j in range(8):
+        if [i, j] == mouse_pos:
+          board.board[i][j].selected = True
+        elif board.board[i][j]:
+          board.board[i][j].selected = False
+        '''      
 
     pygame.display.flip()
     clock.tick(60)
